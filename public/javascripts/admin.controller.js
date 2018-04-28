6b10d83b4fdbdbca06ed40f12adf0f5d12803838
Darpan Jyoti Bora <darpanjyoti.bora@gmail.com>
2018-04-28 15:37:03 +0530

    Integrated paypal gateway

--- public/javascripts/admin.controller.js
+++ public/javascripts/admin.controller.js
@@ -1,13 +1,39 @@
 angular.module('mainApp', ['ngMaterial', 'ngMessages'])
     .controller('AppCtrl', AppCtrl);
 
-function AppCtrl($scope, $mdToast, $mdDialog, $window) {
+function AppCtrl($scope, $mdToast, $mdDialog, $window, $http) {
 
     $scope.imagePath = 'images/washedout.png';
 
     $scope.currentNavItem = 'page2';
 
     $scope.goto = function(page) {};
+
+    $scope.user = {
+        name: '',
+        email: '',
+        phone: '',
+        address: ''
+    };
+
+    $scope.pay = function(course) {
+
+        var userDetails = {
+            name: $scope.user.name,
+            email: $scope.user.email,
+            phone: $scope.user.phone,
+            address: $scope.user.address
+        }
+
+        $http({
+            url: 'https://course-store.herokuapp.com/pay',
+            method: 'POST',
+            data: userDetails
+        }).then(function(httpResponse) {
+            console.log('response:', httpResponse);
+        })
+
+    }
 
     $scope.buy = function(course) {
         console.log(course);
--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,14 @@ var express = require('express');
 var router = express.Router();
 var nodemailer = require('nodemailer');
 
+var paypal = require('paypal-rest-sdk');
+
+paypal.configure({
+    'mode': 'sandbox', //sandbox or live
+    'client_id': process.env.CLIENT_ID,
+    'client_secret': process.env.CLIENT_SECRET
+});
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
     res.render('index');
@@ -11,5 +19,55 @@ router.get('/:id/payment', function(req, res, next) {
     var id = req.params.id;
     res.render('payment', { id: id });
 });
+
+router.get('/success', function(req, res, next) {
+    res.render('success');
+})
+
+router.post('/pay', function(req, res, next) {
+
+    var userDetails = req.body
+
+    console.log(userDetails);
+
+    var create_payment_json = {
+        "intent": "sale",
+        "payer": {
+            "payment_method": "paypal"
+        },
+        "redirect_urls": {
+            "return_url": "https://course-store.herokuapp.com/success",
+            "cancel_url": "https://course-store.herokuapp.com/"
+        },
+        "transactions": [{
+            "item_list": {
+                "items": [{
+                    "name": "Course",
+                    "sku": "0001",
+                    "price": "1.00",
+                    "currency": "INR",
+                    "quantity": 1
+                }]
+            },
+            "amount": {
+                "currency": "INR",
+                "total": "1.00"
+            },
+            "description": "Pay for the course"
+        }]
+    };
+
+    paypal.payment.create(create_payment_json, function(error, payment) {
+        if (error) {
+            throw error;
+        } else {
+            for (var i = 0; i < payment.links.length; i++) {
+                if (payment.links[i].rel === 'approval_url') {
+                    res.redirect(payment.links[i].href);
+                }
+            }
+        }
+    });
+})
 
 module.exports = router;
